380978e53af3cf6b0e0a3961b184cdf661ae8141
Madde Lundström <madde.lundstrom@edu.edugrade.se>
2022-10-04 16:21:05 +0200

    Färdig uppgift

--- OOP_Basics/Program.cs
+++ OOP_Basics/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Madde Lundström NET22
+using System;
 
 namespace OOP_Basics
 {
@@ -6,45 +7,45 @@ namespace OOP_Basics
     {
         static void Main(string[] args)
         {
-            _ = new Circle(5);
+            _ = new Circle(5);                                                              // Instance of class Circle with argument for radius
+            Console.WriteLine();                                                            // For cleaner look in output
+            _ = new Circle(6);                                                              // "_" because VS told me to do so 
+            Console.WriteLine();                                                            // first I wrote Circle C1 = new Circle(6);
+                                                                                            // but was told since C1 wasnt used I should use _
+            _ = new Triangle(4);                                                            
             Console.WriteLine();
-            _ = new Circle(6);
-            Console.WriteLine();
-
-            _ = new Triangle(4);
-            Console.WriteLine();
-            _ = new Triangle(9);
+            _ = new Triangle(9);                                                            // Instance of class Triangle with argument for side
         }
     }
-    public class Circle
+    public class Circle                                                                     // New class
     {
-        public float pi = 3.14f;
-        public int radius;
-        public Circle(int radius)
+        public float pi = 3.14f;                                                            // Public class field
+        public int radius;                                                                  // Public class field
+        public Circle(int radius)                                                           // Constructor of Circle class with int parameter
         {
-            this.radius = radius;
-            Console.Write("En cirkel med radie {0} cm har arean: ", radius);
-            Console.WriteLine(GetArea(radius) + " cm");
+            this.radius = radius;                                                           // Makes so the global variable/field is reached and the same as local
+            Console.Write("En cirkel med radie {0} cm har arean: ", radius);                
+            Console.WriteLine(GetArea(radius) + " cm");                                     
             Console.Write("En cirkel med radie {0} cm har omkretsen: ", radius);
             Console.WriteLine(GetCircum(radius) + " cm");
         }
-        float GetArea(int radius)
+        float GetArea(int radius)                                                           // Method to calculate area based on radius
         {
             this.radius = radius;
-            float area = radius * radius * pi;
-            return area;
+            float area = radius * radius * pi;                                              // Area calculation
+            return area;                                                                    // Returns area calculation
         }
-        float GetCircum(int radius)
+        float GetCircum(int radius)                                                         // Method to calculate circumference based on radius
         {
             this.radius = radius;
-            float circum = 2 * pi * radius;
-            return circum;
+            float circum = 2 * pi * radius;                                                 // Circumference calculation
+            return circum;                                                                  // Returns circumference calculation
         }  
     }
     public class Triangle
     {
-        public int side;
-        public Triangle(int side)
+        public int side;                                                                    // Public class field
+        public Triangle(int side)                                                           // Constructor of Triangle class with int parameter
         {
             this.side = side;
             Console.Write("En triangel med {0} cm långa sidor har arean: ", side);
@@ -52,17 +53,17 @@ namespace OOP_Basics
             Console.Write("En triangel med {0} cm långa sidor har omkretsen: ", side);
             Console.WriteLine(GetCircum(side) + " cm");
         }
-        float GetArea(int side)
+        float GetArea(int side)                                                             // Method to calculate area based on side
         {
             this.side = side;
-            float area = side * side / 2;
-            return area;
+            float area = side * side / 2;                                                   // Area calculation
+            return area;                                                                    // Returns area calculation
         }
-        float GetCircum(int side)
+        float GetCircum(int side)                                                           // Method to calculate circumference based on side
         {
             this.side = side;
-            float circum = side * 3;
-            return circum;
+            float circum = side * 3;                                                        // Circumference calculation
+            return circum;                                                                  // Returns circumference calculation
         }
     }
 }
